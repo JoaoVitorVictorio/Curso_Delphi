fbf60a7c0f05ae3937c1a6dcb7104bdf091f4341
João Vitor Victorio <joaovitorvictorio@gmail.com>
2024-10-19 11:05:50 -0300

    Adding operators to the calculator

--- Calculadora/U_Calculadora.pas
+++ Calculadora/U_Calculadora.pas
@@ -6,7 +6,7 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
   FMX.Controls.Presentation, FMX.StdCtrls, System.Math.Vectors, FMX.Controls3D,
-  FMX.Layers3D, FMX.Layouts;
+  FMX.Layers3D, FMX.Layouts, FMX.Edit;
 
 type
   Tfrm_calculadora = class(TForm)
@@ -27,6 +27,15 @@ type
     btn_somar: TButton;
     btn_subtrair: TButton;
     caixa_botoes: TLayout;
+    caixa_topo: TLayout;
+    txt_numero1: TEdit;
+    lb_operador: TLabel;
+    txt_numero2: TEdit;
+    procedure btn_subtrairClick(Sender: TObject);
+    procedure btn_somarClick(Sender: TObject);
+    procedure btn_multiplicarClick(Sender: TObject);
+    procedure btn_dividirClick(Sender: TObject);
+    procedure btn_igualClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -39,5 +48,32 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure Tfrm_calculadora.btn_dividirClick(Sender: TObject);
+begin
+  lb_operador.Text := '/';
+end;
+
+procedure Tfrm_calculadora.btn_igualClick(Sender: TObject);
+  var n1, n2 : double;
+begin
+  n1 := StrToFloat(txt_numero1.Text);
+  n2 := StrToFloat(txt_numero1.Text);
+end;
+
+procedure Tfrm_calculadora.btn_multiplicarClick(Sender: TObject);
+begin
+  lb_operador.Text := '*';
+end;
+
+procedure Tfrm_calculadora.btn_somarClick(Sender: TObject);
+begin
+  lb_operador.Text := '+';
+end;
+
+procedure Tfrm_calculadora.btn_subtrairClick(Sender: TObject);
+begin
+  lb_operador.Text := '-';
+end;
 
 end.
